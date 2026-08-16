3c774e68c0a1f97a1b773a787901dbc2c64db906
Dharmraj06 <vagheladharmraj06@gmail.com>
2026-08-16 19:07:23 +0000

    started project

---
+++ build/CMakeFiles/rate_limiter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rate_limiter.dir/src/main.cpp.o"
+  "CMakeFiles/rate_limiter.dir/src/main.cpp.o.d"
+  "rate_limiter"
+  "rate_limiter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rate_limiter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Rate Limiter\n";
+    return 0;
+}
