ca1795e90d995f637965c1f9d71ee7930491610c
Dharmraj06 <vagheladharmraj06@gmail.com>
2026-08-17 13:54:36 +0000

    updated the tests for sliding window

--- tests/slidingWindowCounterLimiterTest.cpp
+++ tests/slidingWindowCounterLimiterTest.cpp
@@ -7,102 +7,27 @@
 using namespace std;
 
 int main(){
-    using Clock = chrono::steady_clock;
+    using Clock=chrono::steady_clock;
 
     {
         slidingWindowCounterLimiter limiter(3,chrono::seconds(10));
-        auto start = Clock::now();
-
-        assert(limiter.allow("clientA",start).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(3)).allowed);
-    }
-
-    {
-        slidingWindowCounterLimiter limiter(5,chrono::seconds(10));
-        auto start = Clock::now();
-
-        assert(limiter.allow("clientA",start).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(3)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(4)).allowed);
-
-        assert(!limiter.allow("clientA",start+chrono::seconds(5)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(10)).allowed);
-    }
-
-    {
-        slidingWindowCounterLimiter limiter(3,chrono::seconds(10));
-        auto start = Clock::now();
+        auto start=Clock::now();
 
         assert(limiter.allow("clientA",start).allowed);
         assert(limiter.allow("clientA",start+chrono::seconds(1)).allowed);
         assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
 
         assert(!limiter.allow("clientA",start+chrono::seconds(3)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(21)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(21)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(21)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(21)).allowed);
-    }
-
-    {
-        slidingWindowCounterLimiter limiter(3,chrono::seconds(10));
-        auto start = Clock::now();
-
-        assert(limiter.allow("clientA",start).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
-
-        assert(limiter.allow("clientB",start).allowed);
-        assert(limiter.allow("clientB",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientB",start+chrono::seconds(2)).allowed);
-
-        assert(!limiter.allow("clientA",start+chrono::seconds(3)).allowed);
-        assert(!limiter.allow("clientB",start+chrono::seconds(3)).allowed);
     }
 
     {
         slidingWindowCounterLimiter limiter(2,chrono::seconds(10));
-        auto start = Clock::now();
-
-        assert(limiter.allow("clientA",start).allowed);
-        assert(limiter.allow("clientA",start).allowed);
-        assert(!limiter.allow("clientA",start).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(5)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(10)).allowed);
-    }
-
-    {
-        slidingWindowCounterLimiter limiter(3,chrono::seconds(10));
-        auto start = Clock::now();
+        auto start=Clock::now();
 
         assert(limiter.allow("clientA",start).allowed);
         assert(limiter.allow("clientA",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
 
-        assert(!limiter.allow("clientA",start+chrono::seconds(10)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(15)).allowed);
-    }
-
-    {
-        slidingWindowCounterLimiter limiter(5,chrono::seconds(10));
-        auto start = Clock::now();
-
-        assert(limiter.allow("clientA",start).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(4)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(6)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(8)).allowed);
-
-        assert(!limiter.allow("clientA",start+chrono::seconds(9)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(10)).allowed);
+        assert(!limiter.allow("clientA",start+chrono::seconds(2)).allowed);
     }
 
     cout<<"All sliding window counter tests passed"<<endl;
--- tests/slidingWindowLimiterTest.cpp
+++ tests/slidingWindowLimiterTest.cpp
@@ -7,141 +7,40 @@
 using namespace std;
 
 int main(){
-    using Clock = chrono::steady_clock;
+    using Clock=chrono::steady_clock;
 
     {
         slidingWindowLimiter limiter(3,chrono::seconds(10));
-        auto start = Clock::now();
+        auto start=Clock::now();
 
         assert(limiter.allow("clientA",start).allowed);
         assert(limiter.allow("clientA",start+chrono::seconds(1)).allowed);
         assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
 
         assert(!limiter.allow("clientA",start+chrono::seconds(3)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(4)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(5)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(9)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(10)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(10)).allowed);
 
         assert(limiter.allow("clientA",start+chrono::seconds(11)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(11)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(12)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(12)).allowed);
     }
 
     {
-        slidingWindowLimiter limiter(3,chrono::seconds(10));
-        auto start = Clock::now();
+        slidingWindowLimiter limiter(2,chrono::seconds(10));
+        auto start=Clock::now();
 
         assert(limiter.allow("clientA",start).allowed);
         assert(limiter.allow("clientA",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
 
-        assert(!limiter.allow("clientA",start+chrono::seconds(3)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(4)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(10)).allowed);
-
-        assert(!limiter.allow("clientA",start+chrono::seconds(10)).allowed);
-    }
-
-    {
-        slidingWindowLimiter limiter(3,chrono::seconds(10));
-        auto start = Clock::now();
-
-        assert(limiter.allow("clientA",start).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
-
-        assert(limiter.allow("clientB",start).allowed);
-        assert(limiter.allow("clientB",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientB",start+chrono::seconds(2)).allowed);
-
-        assert(!limiter.allow("clientA",start+chrono::seconds(3)).allowed);
-        assert(!limiter.allow("clientB",start+chrono::seconds(3)).allowed);
-
-        assert(limiter.allow("clientC",start).allowed);
-        assert(limiter.allow("clientC",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientC",start+chrono::seconds(2)).allowed);
-        assert(!limiter.allow("clientC",start+chrono::seconds(3)).allowed);
-    }
-
-    {
-        slidingWindowLimiter limiter(1,chrono::seconds(10));
-        auto start = Clock::now();
-
-        assert(limiter.allow("clientA",start).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(1)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(9)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(10)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(11)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(20)).allowed);
-    }
-
-    {
-        slidingWindowLimiter limiter(5,chrono::seconds(10));
-        auto start = Clock::now();
-
-        assert(limiter.allow("clientA",start).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(3)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(4)).allowed);
-
-        assert(!limiter.allow("clientA",start+chrono::seconds(5)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(9)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(10)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(10)).allowed);
+        assert(!limiter.allow("clientA",start+chrono::seconds(2)).allowed);
 
         assert(limiter.allow("clientA",start+chrono::seconds(11)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(11)).allowed);
     }
 
     {
         slidingWindowLimiter limiter(3,chrono::seconds(10));
-        auto start = Clock::now();
+        auto start=Clock::now();
 
         assert(limiter.allow("clientA",start).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(4)).allowed);
-
-        assert(!limiter.allow("clientA",start+chrono::seconds(5)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(12)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(12)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(14)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(14)).allowed);
-
-        assert(limiter.allow("clientA",start+chrono::seconds(15)).allowed);
-        assert(!limiter.allow("clientA",start+chrono::seconds(15)).allowed);
-    }
-
-    {
-        slidingWindowLimiter limiter(3,chrono::seconds(10));
-        auto start = Clock::now();
-
-        assert(limiter.allow("clientA",start).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientA",start+chrono::seconds(2)).allowed);
-
         assert(limiter.allow("clientB",start).allowed);
-        assert(limiter.allow("clientB",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientB",start+chrono::seconds(2)).allowed);
-
         assert(limiter.allow("clientC",start).allowed);
-        assert(limiter.allow("clientC",start+chrono::seconds(1)).allowed);
-        assert(limiter.allow("clientC",start+chrono::seconds(2)).allowed);
-
-        assert(!limiter.allow("clientA",start+chrono::seconds(3)).allowed);
-        assert(!limiter.allow("clientB",start+chrono::seconds(3)).allowed);
-        assert(!limiter.allow("clientC",start+chrono::seconds(3)).allowed);
 
         assert(limiter.allow("clientA",start+chrono::seconds(10)).allowed);
         assert(limiter.allow("clientB",start+chrono::seconds(10)).allowed);
